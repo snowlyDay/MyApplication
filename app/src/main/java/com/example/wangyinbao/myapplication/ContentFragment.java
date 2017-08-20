5b64039cecffbba90bfc928942dd8d10f24166c6
yinbao <snowlydaymail@163.com>
2017-08-20 17:40:27 +0800

    1

---
+++ app/src/main/java/com/example/wangyinbao/myapplication/ContentFragment.java
@@ -0,0 +1,89 @@
+package com.example.wangyinbao.myapplication;
+
+import android.annotation.SuppressLint;
+import android.support.v4.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import yalantis.com.sidemenu.interfaces.ScreenShotable;
+
+/**
+ * Created by wangyinbao on 2017/8/20.
+ */
+
+@SuppressLint("ValidFragment")
+public class ContentFragment extends Fragment implements ScreenShotable {
+
+    public static final String CLOSE = "Close";
+    public static final String BUILDING = "Building";
+    public static final String BOOK = "Book";
+    public static final String PAINT = "Paint";
+    public static final String CASE = "Case";
+    public static final String SHOP = "Shop";
+    public static final String PARTY = "Party";
+    public static final String MOVIE = "Movie";
+
+
+
+    private View containerView;
+    private int res;
+    private ImageView mImageView;
+    private Bitmap bitmap;
+
+    public static ContentFragment newInstatnce(int resId){
+        ContentFragment contentFragment = new ContentFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(Integer.class.getName(),resId);
+        contentFragment.setArguments(bundle);
+        return contentFragment;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.containerView = view.findViewById(R.id.container);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        res = getArguments().getInt(Integer.class.getName());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mImageView = (ImageView) rootView.findViewById(R.id.image_content);
+        mImageView.setClickable(true);
+        mImageView.setFocusable(true);
+        mImageView.setImageResource(res);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void takeScreenShot() {
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                Bitmap bitmap = Bitmap.createBitmap(containerView.getWidth(),
+                        containerView.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                containerView.draw(canvas);
+                ContentFragment.this.bitmap = bitmap;
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        return null;
+    }
+}
